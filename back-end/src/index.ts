eae5df76dd19ff2db31262c8c75e184be965dbcd
Tran Thanh binh <leon.kieu@gtintel.io>
2021-10-20 23:21:43 +0700

    fix fe, add mongo module

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import mongoose from 'mongoose';
+
 const app = express()
 const port = 3000
 
--- front-end/src/components/Header/header.tsx
+++ front-end/src/components/Header/header.tsx
@@ -1,99 +1,78 @@
+import "./header.css";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import Divider from "@mui/material/Divider";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+import { menuStyle } from '../../utils/common';
 
-import './header.css'
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import Divider from '@mui/material/Divider';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
-// import <Link> from "react-router-dom";
+export const Header = () => {
+  const [item, setItem] = React.useState(null);
+  const open = Boolean(item);
+  const handleClick = (event: any) => {
+    setItem(event.currentTarget);
+  };
+  const handleClose = () => {
+    setItem(null);
+  };
 
-
-
-
-
-function Header() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event: any) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
   return (
     <div>
-        <nav>
-                <div className="nav-left">
-                    <a href="/">
-                        <img className="logo" src="https://smarttrain.edu.vn/assets/uploads/2016/05/tdt-logo.jpg" alt="LogoTDT" />
-                        <p>Tôn Đức Thắng University</p>
-                    </a>
-                </div>
-                <div className="nav-right">
-                    <IconButton className="online" onClick={handleClick} size="small" sx={{ ml: 2 }}>
-                        <Avatar src="https://xaydunghoanghung.com/wp-content/uploads/2020/11/JaZBMzV14fzRI4vBWG8jymplSUGSGgimkqtJakOV.jpeg"></Avatar>
-                    </IconButton>
-                        
-                    <Menu
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
-                        onClick={handleClose}
-                        PaperProps={{
-                            elevation: 0,
-                            sx: {
-                                overflow: 'visible',
-                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                                mt: 1.5,
-                                '& .MuiAvatar-root': {
-                                width: 32,
-                                height: 32,
-                                ml: -0.5,
-                                mr: 1,
-                                },
-                                '&:before': {
-                                content: '""',
-                                display: 'block',
-                                position: 'absolute',
-                                top: 0,
-                                right: 14,
-                                width: 10,
-                                height: 10,
-                                bgcolor: 'background.paper',
-                                transform: 'translateY(-50%) rotate(45deg)',
-                                zIndex: 0,
-                                },
-                            },
-                        }}
-                        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                    >
-                        <MenuItem>
-                        {/* <Link to="/about">About</Link> */}
-                            <Avatar /> Profile
-                        </MenuItem>
-                        <Divider />
-                        <MenuItem>
-                            <ListItemIcon>
-                                <Settings fontSize="small" />
-                            </ListItemIcon>
-                            Settings
-                        </MenuItem>
-                        <MenuItem>
-                            <ListItemIcon>
-                                <Logout fontSize="small" />
-                            </ListItemIcon>
-                            Logout
-                        </MenuItem>
-                    </Menu>
-                </div>
-        </nav>
+      <nav>
+        <div className="nav-left">
+          <a href="/">
+            <img
+              className="logo"
+              src="https://smarttrain.edu.vn/assets/uploads/2016/05/tdt-logo.jpg"
+              alt="LogoTDT"
+            />
+            <p>Tôn Đức Thắng University</p>
+          </a>
+        </div>
+        <div className="nav-right">
+          <IconButton
+            className="online"
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+          >
+            <Avatar src="https://xaydunghoanghung.com/wp-content/uploads/2020/11/JaZBMzV14fzRI4vBWG8jymplSUGSGgimkqtJakOV.jpeg"></Avatar>
+          </IconButton>
+
+          <Menu
+            anchorEl={item}
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            PaperProps={menuStyle}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          >
+            <MenuItem>
+              {/* <Link to="/about">About</Link> */}
+              <Avatar /> Profile
+            </MenuItem>
+            <Divider />
+            <MenuItem>
+              <ListItemIcon>
+                <Settings fontSize="small" />
+              </ListItemIcon>
+              Settings
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>
+                <Logout fontSize="small" />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
+        </div>
+      </nav>
     </div>
   );
 }
 
-export default Header;
