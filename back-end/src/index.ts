fa3defe8c2d5dd5c8cda662afdc6b642772fe84b
Tran Thanh binh <leon.kieu@gtintel.io>
2021-11-06 10:05:12 +0700

    add basic structure

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express'
 import mongoose from 'mongoose';
 
+// await mongoose.connect('mongodb://localhost:27017/web-nang-cao')
+
+const mongodbUrl = 'mongodb://localhost:27017/web-nang-cao'
+
 const app = express()
 const port = 3000
 
