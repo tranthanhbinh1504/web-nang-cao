6a4f90613d724932661a06f564bef6026503751b
TaiNT <56869553+tantai040699@users.noreply.github.com>
2021-12-18 14:09:08 +0700

    update user form UI

--- front-end/src/pages/InApp/User/index.tsx
+++ front-end/src/pages/InApp/User/index.tsx
@@ -1,31 +1,60 @@
 import React, { useState } from 'react'
-import Sidebar from '../../../components/Sidebar/sidebar'
 import Footer from '../../../components/Footer/'
 import { Modal } from 'react-bootstrap'
-import { Button } from 'react-bootstrap'
+import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import Select from '@mui/material/Select'
-import OutlinedInput from '@mui/material/OutlinedInput'
 import { useTheme } from '@mui/material/styles'
 import './style.css'
 import InfiniteScroll from 'react-infinite-scroll-component'
-import { useForm } from 'react-hook-form'
+import { useForm,Controller } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
-
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
 const drawerWidth = 240
 
 //schema validate of Add User Modal
-const schema = yup.object().shape({
-  fullname: yup.string()
-    .required('Your name is required')
-    .max(32, 'Maximum 32 characters')
-    .min(8, 'Minimum 8 characters'),
-})
+interface AddUserModal {
+  fullname: string
+  email: string
+  department: string
+}
 
+const schema = yup.object({
+  fullname: yup.string().required(),
+  email: yup.string().required(),
+  department: yup.string().required(),
+}).required()
+
+const Userdata = [
+  {
+    // id: '1',
+    fullname: 'Nguyễn Tấn Tài',
+    email: 'tantai9991@gmail.com',
+    department: 'CNTT,KTPM',
+  },
+  {
+    // id: '2',
+    fullname: 'Nguyễn Khải',
+    email: 'tantai9991@gmail.com',
+    department: 'CNTT,KTPM',
+  },
+  {
+    // id: '3',
+    fullname: 'Nguyễn abc',
+    email: 'khai@gmail.com',
+    department: 'CNTT,KTPM',
+  },
+  {
+    // id: '4',
+    fullname: 'Nguyễn xst',
+    email: 'tantai9991@gmail.com',
+    department: 'CNTT,KTPM',
+  },
+]
 export default function UserAdmin() {
   //infinity scroll
   // show or hide add user modal
@@ -33,19 +62,26 @@ export default function UserAdmin() {
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
   //get value from Add user Form modal
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: yupResolver(schema) })
-
-  const onSubmit = (data: any) => {
+  const { register, handleSubmit,setValue, formState: { errors } } = useForm<AddUserModal>({
+    resolver: yupResolver(schema)
+  })
+  const onSubmit = (data: AddUserModal) =>{
     console.log(data)
+    Userdata.push(data)
   }
+  const [age, setAge] = React.useState('')
+  const handleChange = (event:any) => {
+    setAge(event.target.value)
+  }
+  //get department value from Form Add User Modal
   // show or hide edit user modal
   const [show1, setShow1] = useState(false)
   const handleClose1 = () => setShow1(false)
-  const handleShow1 = () => setShow1(true)
+  const handleShow1 = (user:any) => {
+    setValue('fullname',user.fullname)
+    console.log(user)
+    setShow1(true)
+  }
   // show or hide delete user modal
   const [show2, setShow2] = useState(false)
   const handleClose2 = () => setShow2(false)
@@ -61,31 +97,7 @@ export default function UserAdmin() {
       },
     },
   }
-  const departments = [
-    'Công Nghệ Thông Tin',
-    'Kế Toán',
-    'Quản trị Kinh doanh',
-  ]
   const theme = useTheme()
-  const [personName, setPersonName] = React.useState([])
-  //multi select
-  const handleChange = (event:any) => {
-    const {
-      target: { value },
-    } = event
-    setPersonName(
-      // On autofill we get a the stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    )
-  }
-  function getStyles(name:any, personName:any, theme:any) {
-    return {
-      fontWeight:
-        personName.indexOf(name) === -1
-          ? theme.typography.fontWeightRegular
-          : theme.typography.fontWeightMedium,
-    }
-  }
   const userAdmin = (
     <div>
       <div className="container useradmin">
@@ -103,20 +115,24 @@ export default function UserAdmin() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td><a href="#">Nguyễn Tấn Tài</a></td>
-              <td>tantai9991@gmail.com</td>
-              <td>CNTT</td>
-              <td>
-                <button className="btn btn-sm btn-outline-primary btn-edit-delete" onClick={handleShow1}>Chỉnh sửa</button>
-                <span>|</span>
-                <button className="btn btn-sm btn-outline-danger btn-edit-delete"  onClick={handleShow2}>Xóa</button>
-              </td>
-            </tr>
+            {Userdata.map((user,index) => {
+              return(
+                <tr key={index+1}>
+                  <td>{index+1}</td>
+                  <td><a href="#">{user.fullname}</a></td>
+                  <td>{user.email}</td>
+                  <td>{user.department}</td>
+                  <td>
+                    <button className="btn btn-sm btn-outline-primary btn-edit-delete" onClick={()=>handleShow1(user)}>Chỉnh sửa</button>
+                    <span>|</span>
+                    <button className="btn btn-sm btn-outline-danger btn-edit-delete"  onClick={handleShow2}>Xóa</button>
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
-        <p>Tổng số người dùng: <strong className="text-danger">5</strong></p>
+        <p>Tổng số người dùng: <strong className="text-danger">{Userdata.length}</strong></p>
       </div>
       {/* add MOdal */}
       <Modal
@@ -137,53 +153,52 @@ export default function UserAdmin() {
               required
               fullWidth
               id="fullname"
-              label="Full Name"
+              label="FullName"
               autoComplete="fullname"
               autoFocus
               {...register('fullname')}
               helperText= {errors.fullname?.message}
             />
-            {/* <TextField
+            <TextField
               className='Login-input'
               margin="normal"
               required
               fullWidth
               id="email"
-              name='email'
               label="Email"
               autoComplete="Email"
               autoFocus
+              {...register('email')}
+              helperText= {errors.email?.message}
             />
-            <InputLabel id="demo-multiple-name-label">Department</InputLabel>
-            <Select
-              labelId="demo-multiple-name-label"
-              id="demo-multiple-name"
-              name='selecter'
-              multiple
-              value={personName}
-              onChange={handleChange}
-              input={<OutlinedInput label="Department" />}
-              MenuProps={MenuProps}
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Department</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="department"
+                label="Department"
+                fullWidth
+                required
+                {...register('department')}
+                onChange={handleChange}
+              >
+                <MenuItem value={'CNTT'}>Công Nghệ Thông Tin</MenuItem>
+                <MenuItem value={'CNTT1'}>Công Nghệ Thông Tin</MenuItem>
+                <MenuItem value={'CNTT2'}>Công Nghệ Thông Tin</MenuItem>
+              </Select>
+            </FormControl>
+            <Button
+              className="login-btn"
+              type="submit"
               fullWidth
-              required
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleClose}
             >
-              {departments.map((department) => (
-                <MenuItem
-                  key={department}
-                  value={department}
-                  style={getStyles(department, personName, theme)}
-                >
-                  {department}
-                </MenuItem>
-              ))}
-            </Select> */}
+                Save
+            </Button>
           </Box>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary">
-            Save
-          </Button>
-        </Modal.Footer>
       </Modal>
 
       {/* edit Modal */}
@@ -197,12 +212,32 @@ export default function UserAdmin() {
         <Modal.Header closeButton>
           <Modal.Title>Chỉnh sửa người dùng</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" onClick={handleClose1}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Modal.Body>
+          <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 1 }}>
+            <TextField
+              className='Login-input'
+              margin="normal"
+              required
+              fullWidth
+              id="fullname"
+              label="FullName"
+              autoComplete="fullname"
+              autoFocus
+              {...register('fullname')}
+              helperText= {errors.fullname?.message}
+            />
+            <Button
+              className="login-btn"
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleClose1}
+            >
+                Save Changes
+            </Button>
+          </Box>
+        </Modal.Body>
       </Modal>
       {/* delete Modal */}
       <Modal
@@ -217,7 +252,7 @@ export default function UserAdmin() {
         </Modal.Header>
         <Modal.Body>Bạn có chắc chắn muốn xóa <strong>Nguyễn Tấn Tài</strong></Modal.Body>
         <Modal.Footer>
-          <Button variant="primary" onClick={handleClose2}>
+          <Button onClick={handleClose2}>
             Delete
           </Button>
         </Modal.Footer>
@@ -226,40 +261,15 @@ export default function UserAdmin() {
   )
   return(
     <div className="user-admin">
-      <Sidebar />
       <Box sx={{ display: 'flex' }}>
         <Box
           component="main"
           sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` },marginLeft:`${drawerWidth}px`}}
           className='box-UserAdmin'
         >
-          {/* <InfiniteScroll
-            dataLength={items.length} //This is important field to render the next data
-            next={fetchMoreData}
-            hasMore={true}
-            loader={<h4>Loading...</h4>}
-            endMessage={
-              <p style={{ textAlign: 'center' }}>
-                <b>Yay! You have seen it all</b>
-              </p>
-            }
-            // below props only if you need pull down functionality
-            refreshFunction={this.refresh}
-            pullDownToRefresh
-            pullDownToRefreshThreshold={50}
-            pullDownToRefreshContent={
-              <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
-            }
-            releaseToRefreshContent={
-              <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
-            }
-          >
-            {userAdmin}
-          </InfiniteScroll> */}
           {userAdmin}
         </Box>
       </Box>
-      <Sidebar />
       <Box sx={{ display: 'flex' }}>
         <Box
           component="main"
